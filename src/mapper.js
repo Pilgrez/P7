a7dad2d244252dfe87a36b14500b1211bf084182
louis <louis.vaisset@protonmail.com>
2021-10-26 15:51:27 +0200

    add multer + map users

--- src/mapper.js
+++ src/mapper.js
@@ -1,4 +1,16 @@
-var fs = require('fs');
+var fs = require('fs'),
+  multer = require('multer');
+
+var multerStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/../public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '.' + file.originalname.split('.').pop())
+  }
+});
+
+const upload = multer({ storage: multerStorage });
 
 var POSTS = require(__dirname + '/posts.js'),
   USERS = require(__dirname + '/users.js');
@@ -17,7 +29,7 @@ function register(app) {
   // register
   app.post("/register", USERS.register);
   // api path
-  app.post("/:cat/:action", map);
+  app.post("/:cat/:action", upload.single('image'), map);
 }
 
 function login(req, res) {
@@ -28,7 +40,6 @@ function login(req, res) {
 
 async function dashboard(req, res) {
   // login user first
-  console.log("DASHBOARD", req.body);
   if (!req.body.hasOwnProperty('email') || !req.body.hasOwnProperty('password')) {
     res.json({result:false, info:"Missing email/password"});
     return;
@@ -40,17 +51,21 @@ async function dashboard(req, res) {
     return;
   }
 
-  console.log("USER", r);
   var html = fs.readFileSync(PATH_HOME, 'utf-8');
+  //html = html.replace('@@USER_AVATAR@@', `/img/users/${r.user.user.userId}.svg`);
 
-  html = html.replace('@@TOKEN@@', r.token);
-  html = html.replace('@@USER_AVATAR@@', `/img/users/${r.user.user.userId}.svg`);
-
+  // return user object without sensitive data
+  delete r.user.user.password;
+  delete r.user.user.isAdmin;
+  html = html.replace('@@USER_DATA@@', JSON.stringify(r.user.user));
   res.send(html);
 }
 
 async function map(req, res) {
-  console.log("API: ", req.params.cat, req.params.action);
+  console.log("API: ", req.params.cat, "->", req.params.action);
+
+  // if image found rebuild body
+  if (!req.headers['content-type'].includes('application/json')) req.body = JSON.parse(JSON.stringify(req.body));
 
   var auth = await USERS.authenticate(req, res);
   if (!auth.result) {
@@ -62,6 +77,9 @@ async function map(req, res) {
   switch (req.params.cat) {
     case "posts":
       body = await POSTS.map(req, res);
+      break;
+    case "users":
+      body = await USERS.map(req, res);
       break;
     default:
       break;
